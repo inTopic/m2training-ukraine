f2fc961011eb41b2740cb5173c939fa177223806
Ruslan <rvoronov@meticulosity.co.za>
2019-10-03 14:30:25 +0300

    1.6. di.xml path#4

--- app/code/Training/TestOM/Model/Test.php
+++ app/code/Training/TestOM/Model/Test.php
@@ -9,23 +9,27 @@ class Test
     private $name;
     private $number;
     private $managerFactory;
+    private $managerCustomImplementation;
 
     public function __construct(
             ManagerInterface $manager,
             $name,
             int $number,
             array $arrayList,
-            \Training\TestOM\Model\ManagerInterfaceFactory $managerFactory
+            ManagerInterfaceFactory $managerFactory,
+            ManagerInterface $managerCustomImplementation
         ) {
         $this->manager = $manager;
         $this->name = $name;
         $this->number = $number;
         $this->arrayList = $arrayList;
         $this->managerFactory = $managerFactory;
+        $this->managerCustomImplementation = $managerCustomImplementation;
     }
 
     public function log()
     {
+        echo '<hr>';
         print_r(get_class($this->manager));
         echo '<br>';
         print_r($this->name);
@@ -39,8 +43,13 @@ class Test
         //that will not be working because we don't have method create implemented
 //        $newManager = $this->manager->create();
         
-        //intead of this we can use factory directly
+        //intead of this we can use factory directly and it will return Training\TestOM\Model\ManagerInterfaceFactory
         print_r(get_class($this->managerFactory));
+        
+        
+        // 1.6
+        echo '<br>';
+        print_r(get_class($this->managerCustomImplementation));
     }
 
 
