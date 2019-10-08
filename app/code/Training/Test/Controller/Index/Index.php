fb42ef673e9a1dec57f941370e0da2f3534b5a8d
Ruslan <rvoronov@meticulosity.co.za>
2019-10-08 03:49:46 +0300

    2.4

---
+++ app/code/Training/Test/Controller/Index/Index.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Training\Test\Controller\Index;
+use Magento\Framework\Controller\Result\RawFactory;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    private $resultRawFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
+    )
+    {
+        $this->resultRawFactory = $resultRawFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $resultRaw = $this->resultRawFactory->create();
+        $resultRaw->setContents('simple text');
+        return $resultRaw;
+    }
+}
