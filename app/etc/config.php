235dc3306700982fa293666878c75f5f26905e30
Ruslan <rvoronov@meticulosity.co.za>
2019-10-17 04:10:46 +0300

    3.9

--- app/etc/config.php
+++ app/etc/config.php
@@ -217,6 +217,7 @@ return [
         'Training_Dependency0' => 1,
         'Training_Dependency' => 1,
         'Training_Plugin' => 1,
+        'Training_Render' => 1,
         'Training_Test' => 1,
         'Training_TestOM' => 1,
         'Training_TestOMOverride' => 1,
